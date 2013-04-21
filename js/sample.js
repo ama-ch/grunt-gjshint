c4e6a19b904bb4b7272f0cd9fb7ae1353718a083
ama-ch <amach97@gmail.com>
2013-04-21 10:26:25 +0900

    Fix sample js.

--- js/sample.js
+++ js/sample.js
@@ -1,10 +1,16 @@
 /**
  * @fileoverview sample script.
  */
+(function() {
+  'use strict';
 
-/**
- * @param {string} name
- */
-var hello = function(name) {
-  console.log(name);
-};
+  /**
+   * @param {string} name
+   */
+  var hello = function(name) {
+    console.log(name);
+  };
+
+  hello();
+
+})();
